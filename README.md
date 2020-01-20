6f879e532d457b101f82f17589d12f9844c64f63
K.Kimura <dotnsf@gmail.com>
2020-01-20 12:47:12 +0900

    fix settings.js and README.md

--- README.md
+++ README.md
@@ -2,6 +2,16 @@
 
 ## Overview
 
+**『名前』を入力すると、その『名前からイメージされる画像』を AI が検索して結果を表示する** というウェブアプリケーションです。
+
+
+## Pre-requites
+
+IBM Cloud のアカウントを取得して、 Watson Language Translator サービスインスタンスを作る必要があります。また、作成したサービスインスタンスに対して、以下のいずれかの手順を行っておく必要があります：
+
+1. （IBM Cloud 上のランタイムとしてアプリケーション・サーバーを稼働させる場合のみ）該当ランタイムから Watson Language Translator サービスをバインド
+
+2. Watson Language Translator サービスの環境変数を参照し、apikey 及び url の値を settings.js 内の該当部分にあらかじめ転記
 
 
 ## Copyright
--- settings.js
+++ settings.js
@@ -4,8 +4,8 @@ exports.watson_lt_url = '';
 
 if( process.env.VCAP_SERVICES ){
   var VCAP_SERVICES = JSON.parse( process.env.VCAP_SERVICES );
-  if( VCAP_SERVICES && VCAP_SERVICES.cloudantNoSQLDB ){
-    exports.db_username = VCAP_SERVICES.cloudantNoSQLDB[0].credentials.username;
-    exports.db_password = VCAP_SERVICES.cloudantNoSQLDB[0].credentials.password;
+  if( VCAP_SERVICES && VCAP_SERVICES.language_translator ){
+    exports.watson_lt_apikey = VCAP_SERVICES.language_translator[0].credentials.apikey;
+    exports.watson_lt_url = VCAP_SERVICES.language_translator[0].credentials.url;
   }
 }
